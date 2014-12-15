48b9d04293370b6daabdabe7abb3a7934cee7349
omarayad1 <omarayad1@gmail.com>
2014-12-15 15:53:51 +0200

    added heroku buildpacks and modified ftp utils to decode url

--- modules/ftp_utils.py
+++ modules/ftp_utils.py
@@ -1,12 +1,12 @@
 import os
 from ftplib import FTP
-import ftplib
+import urllib
 
 
 def download_file_to_tmp(filename):
     host = os.environ["FTP_HOST"]
     username = os.environ["FTP_USERNAME"]
-    password = os.environ["FTP_PASSWORD"]
+    password = urllib.unquote(os.environ["FTP_PASSWORD"])
     ftp = FTP(host, username, password)
     ftp.cwd("ovide-static")
     ftp.retrbinary("RETR %s" % filename, open('./tmp/%s' % filename, 'wb').write)
@@ -16,7 +16,7 @@ def download_file_to_tmp(filename):
 def upload_file_from_tmp(filename):
     host = os.environ["FTP_HOST"]
     username = os.environ["FTP_USERNAME"]
-    password = os.environ["FTP_PASSWORD"]
+    password = urllib.unquote(os.environ["FTP_PASSWORD"])
     ftp = FTP(host, username, password)
     ftp.cwd("ovide-static")
     ftp.storlines("STOR %s" % filename, open('./tmp/%s' % filename, 'r'))
